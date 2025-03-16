e6e2763df763d51571b99802c227f969344c671c
Seth Christopher <seth.christopher93@gmail.com>
2025-03-15 21:41:18 -0400

    checkboxes and slider added.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #app.py
-
 #importing the necessary libraries
 import streamlit as st
 import pandas as pd
@@ -58,13 +57,28 @@ spotify_data = spotify_data.drop(
 spotify_data['release_date'] = pd.to_datetime(spotify_data['release_date'])
 spotify_data['release_year'] = spotify_data['release_date'].dt.year
 
-
-
+# Create streamlit header
 st.header("2024 Spotify Top Songs Analysis")
+
+# Create streamlit slider and supporting dataframe for histogram
+min_year = int(spotify_data['release_year'].min())
+max_year = int(spotify_data['release_year'].max())
+
+year_range = st.slider(
+    'Select Release Year Range',
+    min_value=min_year,
+    max_value=max_year,
+    value=(min_year, max_year)
+)
+
+filtered_histodata = spotify_data[
+    (spotify_data['release_year'] >= year_range[0]) &
+    (spotify_data['release_year'] <= year_range[1]) 
+]
 
 # Create plotly express histogram showing distribution of most streamed songs in 2024 by release year]
 fig = px.histogram(
-    spotify_data,
+    filtered_histodata,
     x='release_year',
     title='Distribution of 2024 Spotify Most Streamed Songs by Release Year',
     labels={'release_year': 'Release Year', 'count': 'Number of Songs'},
@@ -79,13 +93,21 @@ fig.update_layout(
 )
 st.plotly_chart(fig)
 
+# Create streamlit checkboxes for removing outlier x-variables
+filter_playlist_reach = st.checkbox('Show only songs with Spotify Playlist Reach < 125000000', value=False)
+
+# Create filter conditions for scat1
+filtered_scat1 = spotify_data.copy()
+if filter_playlist_reach:
+    filtered_scat1 = filtered_scat1[filtered_scat1['spotify_playlist_reach'] < 125000000]
+
 # Create scatter plot 1
 scat1 = px.scatter(
-    spotify_data,
+    filtered_scat1,
     x='spotify_playlist_reach',
     y='spotify_streams',
     labels={'spotify_playlist_reach': 'Spotify Playlist Reach', 'spotify_streams': 'Spotify Streams'},
-    title='Spotify Playlist Reach vs. Spotify Streams)',
+    title='Spotify Playlist Reach vs. Spotify Streams',
     template='plotly_dark',
     hover_name='track',
     hover_data={'artist': True, 'track_score': True},
@@ -93,9 +115,17 @@ scat1 = px.scatter(
 scat1.update_traces(marker=dict(size=12, opacity=0.8))
 st.plotly_chart(scat1)
 
+# Create streamlit checkboxes for removing outlier x-variables
+filter_tiktok_posts = st.checkbox('Show only songs with TikTok Posts < 15000000', value=False)
+
+# Create filter conditions for scat2
+filtered_scat2 = spotify_data.copy()
+if filter_tiktok_posts:
+    filtered_scat2 = filtered_scat2[filtered_scat2['tiktok_posts'] < 15000000]
+
 # Create scatter plot 2
 scat2 = px.scatter(
-    spotify_data,
+    filtered_scat2,
     x='tiktok_posts',
     y='spotify_streams',
     labels={'tiktok_posts': 'TikTok Posts', 'spotify_streams': 'Spotify Streams'},
